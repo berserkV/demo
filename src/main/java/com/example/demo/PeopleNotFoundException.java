e334ba4e09e05f5f3e4445ff56f1d87da4fb6916
diego-hv <diego.hv@globant.com>
2020-02-26 15:15:18 -0600

    Checkstyle fixed

--- src/main/java/com/example/demo/PeopleNotFoundException.java
+++ src/main/java/com/example/demo/PeopleNotFoundException.java
@@ -2,6 +2,11 @@ package com.example.demo;
 
 public class PeopleNotFoundException extends Exception {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     public PeopleNotFoundException(String message) {
         super(message);
     }
